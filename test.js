eebd5cf983d6563ac2d10aa53c99197c77028c04
makhan tarade <makhan@techflux.in>
2024-10-23 10:34:53 +0530

    update code

--- test.js
+++ test.js
@@ -8,11 +8,12 @@ billing_amount
 
 
 // user rewards after payment ---done
-// commition resto and superadmin
 // banner image on signup time ---done
 // city only mumbai other delete---done
 // get booking ---done
-// new signup mail send superadmin and resto ---done - rework
 // booking main- superadmin, resto, customer--- done
+
 //restoraunt registration time bannerimage not uploaded
+// new signup mail send superadmin and resto ---done - rework
+// commition resto and superadmin
 // booking time check time schedule
